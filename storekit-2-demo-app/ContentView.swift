c3218143b23838009f82a818de7f8e0ef3b18213
AisultanAskarov <36818367+AisultanAskarov@users.noreply.github.com>
2024-01-11 11:41:25 +0300

    Initial Commit

---
+++ storekit-2-demo-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  storekit-2-demo-app
+//
+//  Created by Aisultan Askarov on 11.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
